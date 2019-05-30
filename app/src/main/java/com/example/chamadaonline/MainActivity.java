209558066aad256673befe85e055ada0563d7688
jjunoo <49094710+jjunoo@users.noreply.github.com>
2019-05-30 09:57:20 -0300

    Atualização Firebase

--- app/src/main/java/com/example/chamadaonline/MainActivity.java
+++ app/src/main/java/com/example/chamadaonline/MainActivity.java
@@ -7,11 +7,18 @@ import android.view.View;
 import android.widget.Button;
 
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
+
 
     @BindView(R.id.btnCadastrar)
     Button btnCadastrar;
