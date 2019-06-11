e058138d77addc02dbd544beac6ff37f4f617a27
jjunoo <49094710+jjunoo@users.noreply.github.com>
2019-06-11 12:38:03 -0300

    Cadastro Usuario
    Update Cadastro no Firebase

--- app/src/main/java/com/example/chamadaonline/Cadastro.java
+++ app/src/main/java/com/example/chamadaonline/Cadastro.java
@@ -6,7 +6,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+//import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
