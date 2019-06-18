000c2bb852ef29f726c59fcabbc38888319171e7
jjunoo <49094710+jjunoo@users.noreply.github.com>
2019-06-18 09:48:59 -0300

    Merge branch 'master' of https://github.com/jjunoo/AppChamadaOnline

--- app/src/main/java/com/example/chamadaonline/PrincipalActivity.java
+++ app/src/main/java/com/example/chamadaonline/PrincipalActivity.java
@@ -8,6 +8,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.sql.Timestamp;
+
 import com.example.chamadaonline.config.ConfiguracaoFirebase;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -30,39 +34,53 @@ public class PrincipalActivity extends AppCompatActivity {
         etPIN = findViewById(R.id.etPIN);
         btnEnviarPIN = findViewById(R.id.btnEnviarPIN);
 
-        btnEnviarPIN.setOnClickListener(new View.OnClickListener() {
+        btnEnviarPIN.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                String pinUsuario = etPIN.getText().toString();
-                String pinServidor = "";
+                final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference pinRef = ConfiguracaoFirebase.getFirebaseDatabase().child("id_chamada").child("rand");
 
-                if (!pinUsuario.isEmpty()){
+                String rand = etPIN.getText().toString();
 
-                    pinServidor = GetPinServidor(pinUsuario);//devolve o código pin
-
+<<<<<<< HEAD
                     if (pinUsuario.equals(pinServidor)){
+=======
+                if (!rand.isEmpty()){
+>>>>>>> 596dfe28af6db10ac66ec0a130a130a74768dbb6
 
-                        //usuario = new Usuarios();
-                        //usuario.setEmail(email);
-                        //usuario.setSenha(senha);
-                        //validarLogin();
 
+                        if(!usuarioPresençaDia()) {
+                        long x=new Date().getTime(); // como pegar data
+                        int y=Integer.parseInt(etPIN.getText().toString()); // campo para mandar data via pin porém ciar uma campo de data 
+                            //aqui tem que fazer todo o update do usuario para que seja realizada a chamada
+
+
+                            etPIN.setText("");
+                            Toast.makeText(PrincipalActivity.this, "Presença do dia confirmada!", Toast.LENGTH_SHORT).show();
+                        }else
+                            Toast.makeText(PrincipalActivity.this, "Usuário com presença no dia já!", Toast.LENGTH_SHORT).show();
                     }else
                         Toast.makeText(PrincipalActivity.this, "Código PIN Inválido!", Toast.LENGTH_SHORT).show();
-                }else
-                    Toast.makeText(PrincipalActivity.this, "Digite o Código PIN!", Toast.LENGTH_SHORT).show();
 
             }
         });
     }
-
-    private String GetPinServidor(String pinUsuario)
+   /* private String GetPinServidor(String rand)
     {
         String retorno = "";
 
         //Toast.makeText(PrincipalActivity.this, "Erro ao buscar o Código PIN" + e.getMessage() , Toast.LENGTH_SHORT).show();
         return retorno;
-    }
-}
+    }*/
+
+
+    private boolean usuarioPresençaDia(){
+        boolean retorno = false;
+
+        //logica para verificar no banco se existir voltar true
+        //se não voltar false
+
+        return retorno;
+    }}
 
