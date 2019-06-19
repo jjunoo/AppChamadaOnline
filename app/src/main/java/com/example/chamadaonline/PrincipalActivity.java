62c01dd64840be42a5ea779ab64c97fc0b2cfa6a
jjunoo <49094710+jjunoo@users.noreply.github.com>
2019-06-18 23:09:46 -0300

    Correções
    Correções do código e método "salvarPresenca()" criado

--- app/src/main/java/com/example/chamadaonline/PrincipalActivity.java
+++ app/src/main/java/com/example/chamadaonline/PrincipalActivity.java
@@ -42,66 +42,60 @@ public class PrincipalActivity extends AppCompatActivity {
             public void onClick(View v) {
                 final FirebaseDatabase database = FirebaseDatabase.getInstance();
                 DatabaseReference pinRef = ConfiguracaoFirebase.getFirebaseDatabase().child("id_chamada").child("rand");
-                DatabaseReference data = ConfiguracaoFirebase.getFirebaseDatabase().child("Chamada").child("Data");
-                String rand = etPIN.getText().toString();
+                String pinref = pinRef.toString();
+                String pinAluno = etPIN.getText().toString();
 
-                if (!rand.isEmpty()){
+                if (!pinAluno.isEmpty()){
 
+                    if (pinref.equals(pinAluno)) {
 
-
-                    if(!usuarioPresencaDia()) {
-                        //long x=new Date().getTime(); // como pegar data
-                        Date date = Calendar.getInstance().getTime();
-                        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
-                        String strDate = dateFormat.format(date);
-                        String strdate =date.toString();
-
-                        //System.out.println("Converted String: " + strDate);
-                        //int y=Integer.parseInt(data.getText().toString()); // campo para mandar data via pin porém ciar uma campo de data
-                        //aqui tem que fazer todo o update do usuario para que seja realizada a chamada
-                        salvarPresenca();
+                        //if (!usuarioPresencaDia()) {
 
 
 
-                    // DATA e PIN estão sendo pegos obteção de dados OK testado
-                        etPIN.setText("");
-                       Toast.makeText(PrincipalActivity.this, "Presença do dia confirmada!", Toast.LENGTH_SHORT).show();
+
+                            //aqui tem que fazer todo o update do usuario para que seja realizada a chamada
+                            salvarPresenca();
+
+
+
+                            Toast.makeText(PrincipalActivity.this, "Presença do dia confirmada!", Toast.LENGTH_SHORT).show();
+                       // } else
+                           // Toast.makeText(PrincipalActivity.this, "Usuário com presença já registrada", Toast.LENGTH_SHORT).show();
                     }else
-                        Toast.makeText(PrincipalActivity.this, "Usuário com presença no dia já!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(PrincipalActivity.this, "O pin digitado é inválido", Toast.LENGTH_SHORT).show();
                 }else
-                    Toast.makeText(PrincipalActivity.this, "Código PIN Inválido!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(PrincipalActivity.this, "Você precisa digitar um pin!", Toast.LENGTH_SHORT).show();
 
             }
         });
     }
-    private String GetPinServidor(String rand)
-    {
-        String retorno = "";
-
-        //Toast.makeText(PrincipalActivity.this, "Erro ao buscar o Código PIN" + e.getMessage() , Toast.LENGTH_SHORT).show();
-        return retorno;
-    }
 
 
-    private boolean usuarioPresencaDia(){
+    /*private boolean usuarioPresencaDia(){
         boolean retorno = false;
 
         //logica para verificar no banco se existir voltar true
         //se não voltar false
 
         return retorno;
-    }
+    }*/
 
     public void salvarPresenca(){
 
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        String strDate = dateFormat.format(date);
+        //String strdate = strDate.toString();
+
         String idUsuario = ConfiguracaoFirebase.getIdUsuario();
         DatabaseReference alunoRef = ConfiguracaoFirebase.getFirebaseDatabase()
-                .child("Chamada").child("Data").child("Presenca");
+                .child("Chamada").child(strDate).child(idUsuario).child("Presenca");
 
-        alunoRef.child(idUsuario)
-                .child("Chamada")
-                .child("Data")
-                .child("Presenca")
+        alunoRef.child("Chamada")
+                .child(strDate)
+                .child(idUsuario)
+                .child("Presente")
                 .setValue(this);
         return;
     }}
